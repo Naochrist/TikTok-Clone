5eb321d46a017e9d9f03b85a115e55bcb5f2fcfd
Zemikime Christopher <zemikimechristopher@gmail.com>
2023-02-07 07:52:51 +0100

    Database-setup

---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'h2u3c3pr',
+    dataset: 'tiktok'
+  }
+})
---
+++ sanity-backend/schemas/index.js
@@ -0,0 +1,25 @@
+
+
+
+// import schemaTypes from "all:part:@sanity/base/schema-type"
+import post from './post';
+import user from './user';
+import comment from "./comment";
+import postedBy from "./postedBy";
+
+export const schemaTypes = [
+ 
+  post,
+  user, 
+  comment, 
+  postedBy,
+]
+
+
+
+
+
+
+
+
+
