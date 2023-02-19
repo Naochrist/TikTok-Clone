2d83ac2f7b5e29aafb484539a678d7e16e99ebeb
Zemikime Christopher <zemikimechristopher@gmail.com>
2023-02-19 22:56:58 +0100

    newly added change

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <div>
-        Hello
-      </div>
+      <h1 className='text-3l font-bold underline'>
+        Hello world
+
+      </h1>
     </>
   )
 }
